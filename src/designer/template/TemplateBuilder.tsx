1173716320f76a68fa54001614b77ab59acf9069
Jan Erik Askjellrud <jan.erik.askjellrud@catenda.no>
2024-06-14 10:37:26 +0200

    bootstrap-react

--- src/designer/template/TemplateBuilder.tsx
+++ src/designer/template/TemplateBuilder.tsx
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap';
 import { Flex } from '../../common/Flex'
 import { useDesignerStore } from '../useDesignerStore'
 import './TemplateBuilder.css'
@@ -8,7 +9,7 @@ export const TemplateBuilder = () => {
   return (
     <Flex vertical fullWidth padding32>
       Name {designer.template.name}
-      <button className="btn btn-primary">Click me</button>
+      <Button>click me2</Button>
     </Flex>
   )
 }
