43324055fcef94d91bf5701a3db66b7921de5e8b
Jan Erik Askjellrud <jan.erik.askjellrud@catenda.no>
2024-06-20 20:16:20 +0200

    wip

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,4 +1,17 @@
+import Cookies from "js-cookie";
+
 export const apiBaseUrl =
   import.meta.env.MODE === "development"
     ? "http://localhost:8081"
     : "https://hub.catenda.com";
+
+export const apiHeader = () => {
+  return {
+    withCredentials: true,
+    headers: {
+      "X-XSRF-TOKEN": Cookies.get("XSRF-TOKEN"), // Replace with your CSRF token header name
+      "Content-Type": "application/json", // Adjust content type as needed
+      Authorization: "Bearer " + Cookies.get("_bs_v2_sess"),
+    },
+  };
+};
--- src/api/use-create-template.ts
+++ src/api/use-create-template.ts
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { apiBaseUrl } from "./urls";
+import { apiBaseUrl, apiHeader } from "./urls";
 import { Template } from "../designer/template/template";
 
 const url = `${apiBaseUrl}/checklist/templates`;
@@ -8,7 +8,7 @@ const url = `${apiBaseUrl}/checklist/templates`;
 export const useCreateTemplate = () => {
   return useMutation<Template, Error, Template>({
     mutationFn: async (template: Template) => {
-      return (await axios.post(url, template)).data;
+      return (await axios.post(url, template, apiHeader())).data;
     },
   });
 };
